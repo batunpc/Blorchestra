b9052c9149505ec6c4f014f9ac0a27b9289b3714
batu <bipci@myseneca.ca>
2022-03-24 18:11:56 -0400

    fix: angular.json budgets warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 /*********************************************************************************
- * WEB422 – Assignment 04
+ * WEB422 – Assignment 05
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students. *
