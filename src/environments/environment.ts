f8b4bf4c7dc3be6b9771de036beca927c4eb4763
batu <bipci@myseneca.ca>
2022-04-08 19:59:34 -0400

    🥑

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   clientID: "0daa019b4a964f67b58b2ed0e141f5d1",
   clientSecret: "773bdc74c77043128772053cc5c8262a",
-  //userAPIBase: "https://batufydoodle.herokuapp.com/api/user",
-  userAPIBase: "http://localhost:8080/api/user",
+  userAPIBase: "https://batufydoodle.herokuapp.com/api/user",
+  //userAPIBase: "http://localhost:8080/api/user",
 };
